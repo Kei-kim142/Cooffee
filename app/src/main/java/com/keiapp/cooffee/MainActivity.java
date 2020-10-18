4c0086003ed0100e431fa43aafb4ea5e347f1982
Kei <off.hakim142@gmail.com>
2020-10-18 09:57:10 +0800

    First

---
+++ app/src/main/java/com/keiapp/cooffee/MainActivity.java
@@ -0,0 +1,33 @@
+package com.keiapp.cooffee;
+
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.fragment.NavHostFragment;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+
+import com.keiapp.cooffee.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ActivityMainBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        View view = binding.getRoot();
+        setContentView(view);
+
+        NavHostFragment navHostFragment =
+                (NavHostFragment) getSupportFragmentManager()
+                        .findFragmentById(R.id.fragment);
+        NavController navController = navHostFragment.getNavController();
+        AppBarConfiguration appBarConfiguration =
+                new AppBarConfiguration.Builder(R.id.homeFragment, R.id.menuFragment,R.id.orderFragment,R.id.cartFragment,R.id.accountFragment).build();
+        NavigationUI.setupWithNavController(binding.bottomNavigationView,navController);
+    }
+}
